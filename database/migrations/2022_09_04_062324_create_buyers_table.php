773b4e05c46082851477939920ec7b136d8d26b6
jgcoder <jjgu.tec@gmail.com>
2022-09-10 17:11:12 -0500

    initial commit

---
+++ database/migrations/2022_09_04_062324_create_buyers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buyers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('dni');
+            $table->string('name',250);
+            $table->string('email',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buyers');
+    }
+};
